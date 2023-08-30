9ab0e885ff9ba17578ac66f7813b717a90c71880
KrisHod <krisgodom@gmail.com>
2023-08-30 18:22:42 -0300

    added new examples

--- src/test/java/com/fdmgroup/test/TestSample.java
+++ src/test/java/com/fdmgroup/test/TestSample.java
@@ -1,43 +1,64 @@
 package com.fdmgroup.test;
 
-import com.fdmgroup.util.DriverUtilities;
+import java.io.File;
+import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class TestSample  {
+import com.fdmgroup.util.DriverUtilities;
+
+public class TestSample {
 	private DriverUtilities driverUtilities;
 
-    private WebDriver driver;
+	private WebDriver driver;
 
-    @Before
-    public void setUp() {
-    	driverUtilities = DriverUtilities.getInstance();
-        driver = driverUtilities.getDriver();
-    }
-    
-    @Test
-    public void test1(){
+	@Before
+	public void setUp() {
+		driverUtilities = DriverUtilities.getInstance();
+		driver = driverUtilities.getDriver();
+	}
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
+	@Test
+	public void test1() throws IOException {
+		driver.manage().window().maximize();
+		
+		driver.get("http://www.google.com");
+		System.out.println("Title of browser is: " + driver.getTitle());
+		System.out.println("URL is: " + driver.getCurrentUrl());
+		// driver.navigate().to(null);
+		driver.navigate().back(); // Navigation commands
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		
+		System.out.println("Browser name : " + ((RemoteWebDriver)driver).getCapabilities().getBrowserName());
+		System.out.println("Browser version : " + ((RemoteWebDriver)driver).getCapabilities().getBrowserVersion());
+		
+		File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); 
+		FileHandler.copy(srcFile, new File("C:\\Users\\Kris\\Desktop\\scrsht1.png"));
 
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
+		// Find the text input element by its name
+		WebElement element = driver.findElement(By.name("q"));
 
-        // Enter something to search for
-        element.sendKeys("Cheese!");
+		// Enter something to search for
+		element.sendKeys("Cheese!");
 
-        // Now submit the form
-        element.submit();
+		// Now submit the form
+		element.submit();
+	}
 
-        //Close the browser
-        driver.quit();
-  
+	@After
+	public void destroy() {
+		driver.quit(); // close the entire browser
+		//driver.close(); // close a specific tab in the browser
+
+	}
 }
-    }
-
-
